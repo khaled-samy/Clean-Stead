2b635027596b8fc0afccdc6556c5bfe285679479
K6A1ED <K6A1ED@DESKTOP-G59K8BT>
2023-09-16 22:15:16 +0300

    update Landing and About pages

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,28 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { ImageSection } from '../components'
-import groub from '../assets/Group.png'
 
 export default function Landing (): JSX.Element {
   return (
-    <div className='max-w-[1240px] mx-auto px-4 mb-12 flex flex-col md:flex-row'>
+    <div className='text-center p-20'>
 
-      {/* Landing Information Section */}
-      <div className='md:w-[50%] m-12'>
-        <div className='flex flex-col md:flex-row items-center mb-12'>
-          <h1 className='font-bold text-5xl pl-2'>من نحن</h1>
-          <img className='mt-4' src={groub} alt="groub" />
-        </div>
-        <p className='max-w-md text-xl text-[#7D7E82]'>
-          هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص العربى، حيث يمكنك أن تولد مثل هذا النص.
-        </p>
-        <Link to="/contact">
-          <button className='mt-8 p-4 px-6 border border-solid border-3 rounded-full border-[#00ADEE] text-xl font-bold text-[#00ADEE] hover:bg-[#00ADEE] hover:text-white hover:border-white'>تواصل معنا</button>
-        </Link>
-      </div>
-
-      {/* Image Section */}
-      <ImageSection />
+   <h1 className='text-center text-3xl'>Landing Page</h1>
     </div>
   )
 }
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -2,8 +2,9 @@ import React from "react"
 
 export default function Services (): JSX.Element {
   return (
-    <div>
-      <h1 className="text-blue-600 text-2xl">Services</h1>
-    </div>
-  );
+    <div className='text-center p-20'>
+
+    <h1 className='text-center text-3xl'>Services Page</h1>
+     </div>
+  )
 }
